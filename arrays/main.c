6de9c20726afb1884cee4ba986e9feb3204438a8
gunjan <singhalgunjan5@gmail.com>
2021-05-09 12:45:53 +0530

    Initial Commit

---
+++ arrays/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  arrays
+//
+//  Created by apple on 09/05/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
